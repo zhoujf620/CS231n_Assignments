b8259c0da2b0c0944d160baad99daf792607443e
zhoujf <zhoujf620@zju.edu.cn>
2019-12-17 22:55:00 +0800

    assignment3

---
+++ assignment3/cs231n/datasets/get_coco_captioning.sh
@@ -0,0 +1,3 @@
+wget -c "http://cs231n.stanford.edu/coco_captioning.zip" -c 
+unzip coco_captioning.zip
+rm coco_captioning.zip
---
+++ assignment3/cs231n/datasets/get_imagenet_val.sh
@@ -0,0 +1,1 @@
+wget -c http://cs231n.stanford.edu/imagenet_val_25.npz
